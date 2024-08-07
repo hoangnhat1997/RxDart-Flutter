7019583d5d01b2d264d183d09e8acd865aab3aae
Phạm Vương Hoàng Nhật <nhat.pham@vongxanh.com>
2024-08-07 22:34:25 +0700

    update key

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart/rxdart.dart';
 import 'counter_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
